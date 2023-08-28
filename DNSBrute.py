48934759b9c3e49d4cc94ca9f0230dfe5394a490
debu8er <121763963+debu8er@users.noreply.github.com>
2023-08-28 09:53:24 -0700

    Update DNSBrute.py

--- DNSBrute.py
+++ DNSBrute.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-
+from urllib.parse import urlparse
 
 parser = argparse.ArgumentParser(description="DnsBrute is a tool for Dns BruteForce")
 
@@ -23,19 +23,36 @@ resolver = options.resolver
 wordlist = options.wordlist
 
 
+domain_parts = domain.split('.')
+
+sdomain = domain_parts[-2] if len(domain_parts) >= 2 else domain_parts[0]
+
+
+
+
 os.system("dig A +short somethingdoesntexist."+domain+" > dig-output")
 
 dig = open("dig-output","r")
 if len(dig.read()) < 1:
-    print("continue")
 
     os.system(f"dnsx -d {domain} -w {wordlist} -r {resolver} -silent -o output")
 
-    os.system(f"subfinder -d {domain} -silent -o subdomains")
+    os.system(f'curl -s "https://crt.sh/?q={domain}&output=json" | jq -r ".[].common_name" | sort -u > {sdomain}-crtsh')
+    os.system(f'subfinder -all -d {domain} -silent > {sdomain}-subfinder')
+    os.system(f'curl -s "https://www.abuseipdb.com/whois/{domain}" -H "user-agent: firefox" -b "abuseipdb_session=YOUR-session" | grep -E "<li>\w.*</li>" | sed -E "s/<\/?li>//g" | sed -e "s/$/.{domain}/" > {sdomain}-abuse')
+    os.system(f'cat {sdomain}-crtsh {sdomain}-subfinder {sdomain}-subfinder | sort -u > {sdomain}-allsub')
 
-    os.system("cat output subdomains | sort -u > output2")
 
-    os.system(f"cat output2 | dnsgen - > output3")
+    os.system(f"cat output {sdomain}-allsub | sort -u > output2")
+
+    with open("output2", "r") as file:
+        if len(file.readlines()) >= 500:
+            os.system("cat output2 | httpx > output2-")
+        else:
+            os.system("cat output2 > output2-")
+
+
+    os.system(f"cat output2- | dnsgen - > output3")
 
     os.system(f"dnsx -d {domain} -w output3 -r {resolver} -silent -o final_output")
 
